ebdff2578614243cea113a8b1b95c5bb0d512b45
luoxiang723 <515088980@qq.com>
2020-08-18 15:47:24 +0800

    打印模板调整

--- utils/date-format.js
+++ utils/date-format.js
@@ -132,3 +132,15 @@ export function tim(currentDay) {
   console.log(tragetTime, '这是一周前日期，格式为2010-01-01')
   return tragetTime;
 }
+
+// 获取当前时分秒
+export function timHMS() {
+  var myDate = new Date();
+  let h = myDate.getHours(); //获取当前小时数(0-23)
+  let m = myDate.getMinutes(); //获取当前分钟数(0-59)
+  let s = myDate.getSeconds(); //获取当前秒数(0-59)
+  if (Number(h) < 10) h = '0' + h
+  if (Number(m) < 10) m = '0' + m
+  if (Number(s) < 10) s = '0' + s 
+  return `${h}:${m}:${s}`
+}
